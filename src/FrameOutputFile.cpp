7bd6bbaaf3705cc499941c687bec155cd2da67b8
Thomas Munoz <thomas.munoz30@gmail.com>
2016-01-30 01:22:54 +0100

    Add random number at the end of the filename (for multiple file)

--- src/FrameOutputFile.cpp
+++ src/FrameOutputFile.cpp
@@ -17,12 +17,17 @@ void FrameOutputFile::save(const cv::Mat frame) {
 }
 
 std::string FrameOutputFile::saveFile(const cv::Mat frame) {
+    std::stringstream ss;
+
     std::chrono::time_point<std::chrono::system_clock> start;
     start = std::chrono::system_clock::now();
 
     std::time_t end_time = std::chrono::system_clock::to_time_t(start);
 
-    std::string file = (std::string) (this->fileBase + std::string(std::ctime(&end_time)) + std::string(".jpg"));
+    ss << this->fileBase << std::ctime(&end_time)
+    << '-' << rand() % 1000 + 0 << ".jpg";
+
+    std::string file = ss.str();
 
     // Remove '\n' in file name caused by ctime()
     file.erase(std::remove(file.begin(), file.end(), '\n'), file.end());
